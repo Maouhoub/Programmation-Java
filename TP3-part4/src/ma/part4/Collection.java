d19b99b2727ae6db0861eabf51485ce34397fae8
Maouhoub <36163115+Maouhoub@users.noreply.github.com>
2018-02-25 15:06:05 +0000

    Update Collection.java

--- TP3-part4/src/ma/part4/Collection.java
+++ TP3-part4/src/ma/part4/Collection.java
@@ -16,7 +16,22 @@ public interface Collection
 	 * @return : boolean pour indiquer le succes ou l'echec de l'operation d'ajout
 	 */
 	Boolean addObject(Object o, int i);
+	
+	
+	/**
+	 * permet de supprimer l'objet o dans l'emplacement(indice) i si c'est ce dernier est valable.
+	 *et l'objet o est égale(egalité par equals redéfinie dans o.getClass.getName()).
+	 * @param o : objet potentiel de la case d'indice i
+	 * @param i : indice ou supprimer l'objet
+	 * @return : boolean pour indiquer le succes ou l'echec de l'operation d'ajout
+	 */
 	Boolean removeObject(Object o, int i);
+	
+	/**
+	 *récupérer l'objet d'indice i si cet indice est valable.
+	 *@param i : indice de l'element à récupérer.
+	 *@return : instance de Object .
+	 */
 	Object getObject(int i);
 
 
